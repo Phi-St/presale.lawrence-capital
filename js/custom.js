f44bcb6cdb24568397104dcb779a9d446c7bd0b2
Phi-St <134805035+Phi-St@users.noreply.github.com>
2024-09-06 01:25:05 +0200

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,26 @@
+// Scroll Coin
+
+window.addEventListener('scroll', function() {
+    const scrollPosition = window.scrollY;
+    document.querySelectorAll('.fast-scroll').forEach(function(coin) {
+        coin.style.transform = `translateY(${-scrollPosition * 0.3}px)`;
+    });
+});
+
+// to get current year
+function getYear() {
+    var currentDate = new Date();
+    var currentYear = currentDate.getFullYear();
+    document.querySelector("#displayYear").innerHTML = currentYear;
+}
+getYear();
+
+/*
+/** google_map js 
+function myMap() {
+    var mapProp = {
+        center: new google.maps.LatLng(40.712775, -74.005973),
+        zoom: 18,
+    };
+    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+}*/
